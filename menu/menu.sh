14d2a12b9feea62a2366825b020d8c60d57ed1f6
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-08 03:57:19 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -105,6 +105,11 @@ export BOLD="\e[1m"
 export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+
 # // Clear
 clear
 clear && clear && clear
