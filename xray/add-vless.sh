189d78d2b0cc7c5815b85a0e761a9f19ff2050f9
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-09 23:17:38 +0800

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -130,9 +130,8 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e "Link UNIFI        : ${vlesslink8}" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Created        : $harini" | tee -a /etc/log-create-user.log
-echo -e "\e[33m         : ${user}" | tee -a /etc/log-create-user.log
-echo -e "Expired On     : $exp" | tee -a /etc/log-create-user.log
+echo -e "\e[33m name      : ${user}" | tee -a /etc/log-create-user.log
+echo -e "\e[33mExpired On : $exp" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
 
