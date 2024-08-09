614ddb218904845d9219ef03b494869b513d9872
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-09 23:09:02 +0800

    Update updatefile.sh

--- updatefile.sh
+++ updatefile.sh
@@ -57,7 +57,7 @@ rm -rf add-vless
 cd /usr/bin
 # menu
 wget -O menu "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu.sh"
-wget -O menu-trial "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/menu-trial.sh"
+wget -O menu-trial "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/trialvless.sh"
 wget -O menu-vmess "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-vmess.sh"
 wget -O menu-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-vless.sh"
 wget -O running "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/running.sh"
@@ -89,7 +89,7 @@ wget -O port-change "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/p
 wget -O certv2ray "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/certv2ray.sh"
 wget -O menu-webmin "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/about.sh"
+wget -O about "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/about.sh"
 wget -O auto-reboot "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/auto-reboot.sh"
 wget -O restarts "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/restarts.sh"
 wget -O bw "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/bw.sh"
@@ -104,7 +104,6 @@ wget -O acs-set "https://raw.githubusercontent.com/KhaiVpn767/v4/main/acs-set.sh
 
 wget -O sshws "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/KhaiVpn767/v4/main/status.sh"
-wget https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
 wget -O /usr/bin/menu "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
 wget -O /usr/bin/backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/backup.sh" && chmod +x /usr/bin/backup
 wget -O /usr/bin/restore "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/restore.sh" && chmod +x /usr/bin/restore
