37d9d8b7743375eb7d9075f98a95a0347af40cba
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-09 21:33:28 +0800

    Update updatefile.sh

--- updatefile.sh
+++ updatefile.sh
@@ -1,107 +1,161 @@
-domain=$(cat /etc/xray/domain)
-TIMES="10"
-CHATID=$(cat /etc/id)
-KEY=$(cat /etc/token)
-URL="https://api.telegram.org/bot$KEY/sendMessage"
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+echo -e " [INFO] Downloading Update File"
+sleep 2
+# hapus menu
+rm -rf menu
+rm -rf menu-trial
+rm -rf menu-vmess
+rm -rf menu-vlesss
+rm -rf running
+rm -rf clearcache
+rm -rf menu-trgo
+rm -rf menu-trojan
+rm -rf menu-sshh
+rm -rf menu-ssh
+rm -rf menu-sshh
+rm -rf usernew
+rm -rf trial
+rm -rf renew
+rm -rf hapus
+rm -rf cek
+rm -rf member
+rm -rf delete
+rm -rf autokill
+rm -rf ceklim
+rm -rf autokilll
+rm -rf tendang
+rm -rf menu-set
+rm -rf menu-domain
+rm -rf add-host
+rm -rf port-change
+rm -rf certv2ray
+rm -rf menu-webmin
+rm -rf speedtest
+rm -rf about
+rm -rf auto-reboot
+rm -rf restart
+rm -rf bw
+rm -rf port-ssl
+rm -rf port-ovpn
+rm -rf xp
+rm -rf acs-set
+rm -rf sshws
+rm -rf status
+rm -rf menu-backup
+rm -rf backup
+rm -rf restore
+rm -rf jam
+rm -rf up
+#rm -rf bot
 
-tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
-none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
-user=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
-uuid=$(cat /proc/sys/kernel/random/uuid)
-masaaktif=1
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (days): " masaaktif
-sed -i '/#vless$/a\#& '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-export vlesslink1="vless://${uuid}@${sts}${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws&sni=$sni#${user}"
-export vlesslink2="vless://${uuid}@${sts}${domain}:$none?path=/vless&encryption=none&host=$sni&type=ws#${user}"
-export vlesslink3="vless://${uuid}@api.useinsider.com:$none?path=/vless&encryption=none&host=${sts}${domain}&type=ws#Server:$creditt-DIGI-APN-${user}"
-export vlesslink4="vless://${uuid}@162.159.134.61:$none?path=/vless&encryption=none&host=${sts}${domain}&type=ws#Server:$creditt-DIGI-BOSSTER-${user}"
-export vlesslink5="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&host=m.pubgmobile.com&type=ws#Server:$creditt-UMOBILE-FUNZ-${user}"
-export vlesslink6="vless://${uuid}@104.17.113.188:$none?path=/vless&encryption=none&host=eurohealthobservatory.who.int.${domain}&type=ws#Server:$creditt-YES-${user}"
-export vlesslink7="vless://${uuid}@104.17.148.22:$none?path=/vless&encryption=none&host=www.speedtest.net.${domain}&type=ws#Server:$creditt-SELCOM-0BASIC-${user}"
-export vlesslink8="vless://${uuid}@104.17.10.12:$none?path=/vless&encryption=none&host=cdn.who.int.${domain}&type=ws#Server:$creditt-UNIFI-${user}"
-systemctl restart xray
-clear
-vless1="$(echo $vlesslink1 | base64 -w 0)"
-vless2="$(echo $vlesslink2 | base64 -w 0)"
-vless3="$(echo $vlesslink3 | base64 -w 0)"
+# download script
+cd /usr/bin
+# menu
+wget -O up "https://raw.githubusercontent.com/KhaiVpn767/v4/main/up.sh"
+wget -O menu "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu.sh"
+wget -O menu-trial "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/menu-trial.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/clearcache.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-trojan.sh"
 
-TEXT="
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>  Premium Vless Account</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Remarks      : </code> <code>${user}</code>
-<code>Domain       : </code> <code>${domain}</code>
-<code>Port TLS     : 443</code>
-<code>Port NTLS    : 80, 8080</code>
-<code>Port GRPC    : 443</code>
-<code>User ID      : </code> <code>${uuid}</code>
-<code>AlterId      : 0</code>
-<code>Security     : auto</code>
-<code>Network      : WS or gRPC</code>
-<code>Path vless   : </code> <code>/vless</code>
-<code>ServiceName  : </code> <code>/vless-grpc</code>
-<code>Expired On : </code> <code>$timer Minutes</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link TLS     :</code> 
-<code>${vless1}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link NTLS    :</code> 
-<code>${vless2}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link GRPC    :</code> 
-<code>${vless3}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-"
+# menu ssh ovpn
+wget -O menu-sshh "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-sshh.sh"
+wget -O usernew "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/delete.sh"
+wget -O autokilll "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/autokilll.sh"
+wget -O ceklim "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/tendang.sh"
 
-curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+# menu system
+wget -O menu-set "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/Fikripps/Ver3/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/auto-reboot.sh"
+wget -O restarts "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/restarts.sh"
+wget -O bw "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/bw.sh"
 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m        Trial Xray/Vless        \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks        : ${user}"
-echo -e "Domain         : ${domain}"
-echo -e "port TLS       : $tls"
-echo -e "port none TLS  : $none"
-echo -e "id             : ${uuid}"
-echo -e "Encryption     : none"
-echo -e "Network        : ws"
-echo -e "Path           : /vless"
-echo -e "Path           : vless-grpc"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link DIGI APN     : ${vlesslink3}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link DIGI-BOSSTER : ${vlesslink4}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link UMOBILE-FUNZ : ${vlesslink5}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link YES          : ${vlesslink6}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link SELCOM-0BASIC: ${vlesslink7}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Link UNIFI        : ${vlesslink8}" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Created        : $harini" | tee -a /etc/log-create-user.log
-echo -e "\e[33m         : ${user}" | tee -a /etc/log-create-user.log
-echo -e "Expired On     : $exp" | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo ""
+# change port
+wget -O port-ssl "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-ovpn.sh"
 
-read -n 1 -s -r -p "Press any key to back on menu"
 
-menu
+wget -O xp "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/KhaiVpn767/v4/main/acs-set.sh"
+
+wget -O sshws "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/sshws.sh"
+wget -O status "https://raw.githubusercontent.com/KhaiVpn767/v4/main/status.sh"
+wget https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/bin/backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/backup.sh" && chmod +x /usr/bin/backup
+wget -O /usr/bin/restore "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/restore.sh" && chmod +x /usr/bin/restore
+wget -O /usr/bin/up "https://raw.githubusercontent.com/KhaiVpn767/v4/main/up.sh" && chmod +x /usr/bin/up
+wget -O jam "https://raw.githubusercontent.com/KhaiVpn767/v4/main/jam.sh"
+wget https://raw.githubusercontent.com/KhaiVpn767/v4/main/udp/udp.sh && bash udp.sh
+wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+
+#install Bot panel
+wget -O bot "https://raw.githubusercontent.com/KhaiVpn767/v4/main/bot.sh" && chmod +x /usr/bin/bot
+
+chmod +x menu
+chmod +x menu-trial
+chmod +x menu-vmess
+chmod +x menu-vless
+chmod +x running
+chmod +x clearcache
+chmod +x menu-trgo
+chmod +x menu-trojan
+
+chmod +x menu-sshh
+chmod +x usernew
+chmod +x trial
+chmod +x renew
+chmod +x hapus
+chmod +x cek
+chmod +x member
+chmod +x delete
+chmod +x autokilll
+chmod +x ceklim
+chmod +x tendang
+
+chmod +x menu-set
+chmod +x menu-domain
+chmod +x add-host
+chmod +x port-change
+chmod +x certv2ray
+chmod +x menu-webmin
+chmod +x speedtest
+chmod +x about
+chmod +x auto-reboot
+chmod +x restarts
+chmod +x bw
+
+chmod +x port-ssl
+chmod +x port-ovpn
+
+chmod +x xp
+chmod +x acs-set
+chmod +x sshws
+chmod +x status
+chmod +x menu-bckp
+chmod +x backup
+chmod +x restore
+chmod +x jam
+echo -e " [INFO] Update Successfully"
+sleep 2
+exit
