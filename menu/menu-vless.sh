f43ffb6e936629657ba2ce608b504de288b0f47a
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-01-15 23:38:01 +0800

    Update menu-vless.sh

--- menu/menu-vless.sh
+++ menu/menu-vless.sh
@@ -90,6 +90,7 @@ echo -e " [\e[36m•2\e[0m] Buat Trial "
 echo -e " [\e[36m•3\e[0m] Ubah Akun Vless "
 echo -e " [\e[36m•4\e[0m] Hapus Akun Vless "
 echo -e " [\e[36m•5\e[0m] Cek User Login Vless "
+echo -e " [\e[36m•6\e[0m] Create Account manual UUID "
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -102,6 +103,7 @@ case $opt in
 3) clear ; renew-vless ; exit ;;
 4) clear ; del-vless ; exit ;;
 5) clear ; cek-vless ; exit ;;
+6) clear ; addvls ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
