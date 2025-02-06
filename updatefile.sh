dc51f2feb39b278b841b8ee8bfd6c9a3df21c7f5
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-02-06 23:13:29 +0800

    Update updatefile.sh

--- updatefile.sh
+++ updatefile.sh
@@ -54,7 +54,7 @@ rm -rf restore
 rm -rf jam
 rm -rf up
 rm -rf trialvless
-rm -rf add-vless
+rm -rf addvless
 #rm -rf bot
 
 # download script
@@ -69,7 +69,6 @@ wget -O clearcache "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/cl
 wget -O menu-trgo "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-trgo.sh"
 wget -O menu-trojan "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-trojan.sh"
 wget -O trialvless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/trialvless.sh"
-wget -O add-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/add-vless.sh"
 wget -O renew-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/renew-vless.sh"
 wget -O cek-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/cek-vless.sh"
 wget -O del-vless "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/del-vless.sh"
@@ -93,7 +92,6 @@ wget -O tendang "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/tendan
 wget -O menu-set "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-set.sh"
 wget -O menu-domain "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/KhaiVpn767/v4/main/xray/certv2ray.sh"
 wget -O menu-webmin "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/speedtest_cli.py"
@@ -104,27 +102,22 @@ wget -O bw "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/bw.sh"
 wget -O m-helium "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/m-helium.sh"
 wget -O autobackup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/autobackup"
 
-# change port
-wget -O port-ssl "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/KhaiVpn767/v4/main/port/port-ovpn.sh"
 
+# change port
+
+
+# file tambahan
 
 wget -O xp "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/KhaiVpn767/v4/main/acs-set.sh"
 
 wget -O sshws "https://raw.githubusercontent.com/KhaiVpn767/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/KhaiVpn767/v4/main/status.sh"
-wget -O /usr/bin/menu "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -0 menu-backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/menu/menu-backup.sh"
-wget -O /usr/bin/backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/backup.sh" && chmod +x /usr/bin/backup
-wget -O /usr/bin/restore "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/restore.sh" && chmod +x /usr/bin/restore
-wget -O /usr/bin/up "https://raw.githubusercontent.com/KhaiVpn767/v4/main/up.sh" && chmod +x /usr/bin/up
+wget -O menu-backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/menu-backup.sh"
+wget -O backup "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/KhaiVpn767/v4/main/backup/restore.sh"
+wget -O bot "https://raw.githubusercontent.com/KhaiVpn767/v4/main/bot.sh"
+
 wget -O jam "https://raw.githubusercontent.com/KhaiVpn767/v4/main/jam.sh"
-#wget -O xraay2 "wget https://raw.githubusercontent.com/KhaiVpn767/v4/main/xraay2.sh && chmod +x xraay2.sh && ./xraay2.sh"
-#wget https://raw.githubusercontent.com/KhaiVpn767/v4/main/udp/udp.sh && bash udp.sh
-#wget https://raw.githubusercontent.com/KhaiVpn767/SlowDnsV1/main/dns2.sh && chmod +x dns2.sh && ./dns2.sh
-
-
 
 chmod +x menu
 chmod +x menu-trial
@@ -136,12 +129,10 @@ chmod +x menu-trgo
 chmod +x menu-trojan
 chmod +x trialvless
 chmod +x addvless
-chmod +x add-vless
 chmod +x renew-vless
 chmod +x cek-vless
 chmod +x del-vless
-chmod +x addvls.sh
-chmod +x addvless.sh
+chmod +x addvls
 
 chmod +x menu-sshh
 chmod +x usernew
@@ -169,19 +160,15 @@ chmod +x bw
 chmod +x m-helium
 chmod +x autobackup
 
-chmod +x port-ssl
-chmod +x port-ovpn
 
 chmod +x xp
 chmod +x acs-set
 chmod +x sshws
 chmod +x status
-chmod +x menu-bckp
 chmod +x menu-backup
 chmod +x backup
 chmod +x restore
 chmod +x jam
-chmod +x xraay2
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
